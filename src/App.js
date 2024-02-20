45d6db065c5e953cac21691d602caf3d51acb808
Karl Tammehoid <tammehoidkarl@gmail.com>
2024-02-20 21:54:03 +0200

    Playlist content

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react"
 // website elements
 import Header from "./Header"
 import Playlist from "./Playlist"
+import Content from "./Content"
 import Footer from "./Footer"
 
 // the app itself
@@ -11,6 +12,7 @@ export default function App() {
     <div>
       <Header />
       <Playlist />
+      <Content />
       <Footer />
     </div>
   )
---
+++ src/Content.js
@@ -0,0 +1,28 @@
+import React from "react"
+
+// website elements
+import Playlist from "./Playlist"
+import PlaylistItem from "./Playlist"
+
+export default function Content() {
+        const Contentstyle = {
+                fontFamily: 'Inter, sans-serif', // Specify Inter as the primary font
+        };
+
+        return (
+                <div>
+                        <div className="Content--container"></div>
+                        <div className="Content--header" style={Contentstyle}>
+                                <h3 className="CH--title">Playlist Name</h3>
+                                <img className="CH--pic" src="./images/No-Picture.png"></img>
+                                <div className="CH--bio">
+                                        <h3>#</h3>
+                                        <h3 className="Bio--Track">Title</h3>
+                                        <h3 className="Bio--Album">Album</h3>
+                                        <h3 className="Bio--Time">Time Added</h3>
+                                        <h3 className="Bio--Clock">🕒</h3>
+                                </div>
+                        </div>
+                </div>
+        )
+}
